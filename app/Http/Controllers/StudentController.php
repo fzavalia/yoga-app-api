d8b17f27bee9da40ba181ea6c60d147e127d0317
Fernando Zavalia <zavaliafernando@gmail.com>
2019-05-07 01:34:34 -0300

    Student show and update

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Student;
+
+class StudentController extends Controller
+{
+    public function show($id) {
+
+        $student = Student::findOrFail($id);
+
+        return $student;
+    }
+
+    public function store(Request $request)
+    { 
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'email' => 'email',
+            'phone_number' => 'string',
+            'dni' => 'int'
+        ]);
+
+        $student = Student::create($validatedData);
+
+        return $student;
+    }
+
+    public function update(Request $request, $id)
+    { 
+        $validatedData = $request->validate([
+            'name' => 'string',
+            'email' => 'email',
+            'phone_number' => 'string',
+            'dni' => 'int'
+        ]);
+
+        $student = Student::findOrFail($id);
+
+        $student->update($validatedData);
+
+        return $student;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/students', 'StudentController@store');
+Route::put('/students/{id}', 'StudentController@update');
+Route::get('/students/{id}', 'StudentController@show');
