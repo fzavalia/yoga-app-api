11f8704b94e643a9d9fb4bc77ee151a43dfc3928
Fernando Zavalia <zavaliafernando@gmail.com>
2019-05-26 04:43:19 -0300

    Add Carbon + YogaClass update date validation

--- app/Http/Controllers/YogaClassController.php
+++ app/Http/Controllers/YogaClassController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\YogaClass;
 use App\Helpers\ControllerHelpers;
+use Carbon\Carbon;
 
 class YogaClassController extends Controller
 {
@@ -36,12 +37,28 @@ class YogaClassController extends Controller
     public function update(Request $request, $id)
     {
         $validatedData = $request->validate([
-            'date' => 'date|unique',
             'student_ids' => 'array',
             'student_ids.*' => 'int'
         ]);
 
         $yogaClass = YogaClass::findOrFail($id);
+
+        // Will only validate date uniqueness when provided date is 
+        // different from the stored date.
+
+        if ($request->has('date')) {
+
+            $receivedDate = Carbon::create($request->date)->format('Y-m-d');
+            $existingDate = Carbon::create($yogaClass->date)->format('Y-m-d');
+
+            if ($receivedDate !== $existingDate) {
+                $validatedData = array_merge($validatedData, $request->validate([
+                    'date' => 'date|unique:yoga_classes',
+                ]));
+            }
+        }
+
+        info($yogaClass->date);
 
         $yogaClass->update($validatedData);
 
