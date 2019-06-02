c98f9cb4e660c38e685b1651667adcc0dcc6fccf
Fernando Zavalia <zavaliafernando@gmail.com>
2019-06-02 18:43:10 -0300

    Fix migration + UserTableSeeder

--- database/migrations/2019_05_26_221309_add_invoiced_column_to_payments_table.php
+++ database/migrations/2019_05_26_221309_add_invoiced_column_to_payments_table.php
@@ -26,7 +26,7 @@ class AddInvoicedColumnToPaymentsTable extends Migration
     public function down()
     {
         Schema::table('payments', function (Blueprint $table) {
-            $table->dropColumn('invoided');
+            $table->dropColumn('invoiced');
         });
     }
 }
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('students')->delete();
+
+        User::create([
+            'name' => 'test',
+            'email' => 'test@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+}
