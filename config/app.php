ad55761ce463c43c27c48ff0ef91cbf062d71eef
Fernando Zavalia <zavaliafernando@gmail.com>
2019-06-22 00:02:15 -0300

    Revert "Validate composed keys uniqueness package"
    This reverts commit 553f0798ecc67d89077219b40f70d4e43b457cb8.

--- config/app.php
+++ config/app.php
@@ -175,9 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        // Validate composite keys
-        Felixkiss\UniqueWithValidator\ServiceProvider::class,
-
     ],
 
     /*
