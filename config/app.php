553f0798ecc67d89077219b40f70d4e43b457cb8
Fernando Zavalia <zavaliafernando@gmail.com>
2019-06-21 23:57:09 -0300

    Validate composed keys uniqueness package

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Validate composite keys
+        Felixkiss\UniqueWithValidator\ServiceProvider::class,
+
     ],
 
     /*
